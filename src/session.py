97b716f50936508cd2b3f6cc4c0a2c4e178a2a74
Will Johnson <willejohns@gmail.com>
2015-10-24 13:51:56 -0400

    repairs

--- src/session.py
+++ src/session.py
@@ -10,13 +10,17 @@ import numpy
 class session(sound.sound):
 
     def __init__(self, name, length, sr=44100):
-	self.tracks = []
+	self.tracks = numpy.array([])
 	self.sr = sr
 	self.length = length
+	self.data = numpy.array([])
 	
     def add_track(self, name):
 	self.tracks.append(track.track(name, self.length, self.sr))
 
     def render(self):
 	#bounce the session to a single audio file
-	return
+	for track in self.tracks:
+	    track.render()
+	self.data = sum(tracks)
+	
